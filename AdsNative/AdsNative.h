ebc97c1139a8b66e65a83475372a39ef1815e395
Kuldeep Kapade <kuldeepkapade@gmail.com>
2014-06-11 19:40:01 -0700

    Added missing SDK

---
+++ AdsNative/AdsNative.h
@@ -0,0 +1,12 @@
+//
+//  AdsNative.h
+//  AdsNative
+//
+//  Created by Santthosh on 6/4/13.
+//  Copyright (c) 2013 AdsNative. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ANAdRequest.h"
+#import "ANSponsoredStory.h"
